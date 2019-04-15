5289aa4ea29e947c3ce64f4bf3a44795590536e9
unknown <sunqixuanno1@126.com>
2019-04-15 20:20:18 +0800

    add TextRNN

--- SentimentAnalysis/run_rnn.py
+++ SentimentAnalysis/run_rnn.py
@@ -3,10 +3,10 @@ import tensorflow as tf
 import time
 import matplotlib.pyplot as plt
 import pandas as pd
-import TextCNN
+import TextRNN
 
 time_start = time.time()
-config = TextCNN.TextcnnConfig()
+config = TextRNN.TextrnnConfig()
 
 cnt = 0
 demo = 4000
@@ -35,8 +35,6 @@ while cnt != demo:  # pos_full = 18601
     label_array[2 * cnt + 1] = [0, 1]
     cnt += 1
 
-vec_array = np.expand_dims(vec_array, -1).astype(np.float32)
-
 train_split = np.random.randint(all, size=int(all*0.8))
 train_x , train_y = vec_array[train_split], label_array[train_split]
 test_x , test_y = vec_array[~train_split], label_array[~train_split]
@@ -51,7 +49,8 @@ def Choose_SGD(round, accuracy_list):
     else:
         return False
 
-cnn = TextCNN.TextCNN(config)
+
+rnn = TextRNN.TextRNN(config)
 train_accuracy_list = []
 test_accuracy_list = []
 with tf.Session() as sess:
@@ -61,29 +60,26 @@ with tf.Session() as sess:
 
     for i in range(config.round):
         train_index = np.random.randint(len(train_x), size=config.mini_batch)
-        '''
         if Choose_SGD(i, train_accuracy_list):
-            sess.run(cnn.SGD, feed_dict={"input_x:0": train_x[train_index],"input_y:0": train_y[train_index]})
+            sess.run(rnn.SGD, feed_dict={"input_x:0": train_x[train_index],"input_y:0": train_y[train_index]})
         else:
-            sess.run(cnn.AdamOptimize, feed_dict={"input_x:0": train_x[train_index], "input_y:0": train_y[train_index]})
-        '''
-        sess.run(cnn.AdamOptimize, feed_dict={"input_x:0": train_x[train_index], "input_y:0": train_y[train_index]})
+            sess.run(rnn.AdamOptimize, feed_dict={"input_x:0": train_x[train_index], "input_y:0": train_y[train_index]})
 
-        train_accuracy = sess.run(cnn.accuracy, feed_dict={"input_x:0": train_x, "input_y:0": train_y})
+        train_accuracy = sess.run(rnn.accuracy, feed_dict={"input_x:0": train_x, "input_y:0": train_y})
         train_accuracy_list.append(train_accuracy)
-        test_accuracy = sess.run(cnn.accuracy, feed_dict={"input_x:0": test_x, "input_y:0": test_y})
+        test_accuracy = sess.run(rnn.accuracy, feed_dict={"input_x:0": test_x, "input_y:0": test_y})
         test_accuracy_list.append(test_accuracy)
 
         if i % 10 == 0:
             print('After %d rounds,accuracy on training set is %s, on testing set is %s' % (i, train_accuracy, test_accuracy))
 
 accuracy = pd.DataFrame(data={"train_accuracy":train_accuracy_list,"test_accuracy":test_accuracy_list})
-accuracy.to_csv("CnnModel_AdamOnly_mini-batch=%s_hidden-size=%s_filter-num=%s_dropout-keep=%s.csv"
-                % (config.mini_batch, config.hidden_size, config.filter_num, config.dropout_keep_prob), encoding="utf-8")
+accuracy.to_csv("LSTMModel_mini-batch=%s_hidden-size=%s_num-layers=%s_dropout-keep=%s.csv"
+                % (config.mini_batch, config.hidden_size, config.num_layers, config.dropout_keep_prob), encoding="utf-8")
 
 plt.plot(range(0, config.round), train_accuracy_list)
 plt.plot(range(0, config.round), test_accuracy_list)
 plt.xlabel("round")
 plt.ylabel('accuracy')
-plt.title('CNN mini-batch=%s' % config.mini_batch)
+plt.title('LSTM mini-batch=%s' % config.mini_batch)
 plt.show()
